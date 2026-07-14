ebba1fd49e2c33d8c0c83cf3da34ae6e689b2295
Binhti <tonyiscoding2003@gmail.com>
2026-07-13 17:15:31 -0700

    init empty

--- Arrays/array.c
+++ Arrays/array.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 typedef struct {
     int *data;      // pointer to the raw array on the heap
     int size;       // number of items currently stored
     int capacity;   // number of items the array can hold
 } Vector;
+
 
 // Pick a starting capacity
 // Minimum is 16
@@ -20,6 +21,22 @@ int choose_capacity(int requested) {
     return capacity;
 }
 
+void vector_init(Vector *v, int requested) {
+    v->size = 0;
+    v->capacity = choose_capacity(requested);
+    v->data = malloc(v->capacity * sizeof(int));
+
+    if (v->data == NULL) {
+        printf("malloc failed\n");
+        exit(1);
+    }
+}
+
+bool is_empty(Vector *v){
+    return v->size == 0;
+    
+}
+
 int size(Vector *v){
     return v->size;
 }
@@ -28,7 +45,9 @@ int capacity(Vector *v){
     return v->capacity;
 }
 
-
+int at(Vector *v, int index) {
+    
+}
 // Creating my own vector class with all functions in C
 int main(void){
     return 0;
